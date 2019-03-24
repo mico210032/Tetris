bb1a9bc38f95bb1bf76ad4df6a5494a9576bc6ef
mico210032 <48892947+mico210032@users.noreply.github.com>
2019-03-25 05:46:23 +0800

    Add files via upload

---
+++ ChinaBlock/ChinaBlock/Form1.Designer.cs
@@ -0,0 +1,385 @@
+﻿namespace ChinaBlock
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.picBackGround = new System.Windows.Forms.PictureBox();
+            this.pic_preView = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.t_score = new System.Windows.Forms.Label();
+            this.游戏设置ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.速度设置ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.较慢ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.慢ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.快ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.较快ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.非常快ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.背景颜色设置ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.方块颜色设置ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.恢复默认设置ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
+            this.退出ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.控制ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.开始ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.暂停ToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.结束ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.重新开始ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.帮助ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.关于ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.操作说明ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.msg = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.picBackGround)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_preView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picBackGround
+            // 
+            this.picBackGround.BackColor = System.Drawing.Color.Tan;
+            this.picBackGround.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.picBackGround.Location = new System.Drawing.Point(72, 40);
+            this.picBackGround.Name = "picBackGround";
+            this.picBackGround.Size = new System.Drawing.Size(200, 400);
+            this.picBackGround.TabIndex = 0;
+            this.picBackGround.TabStop = false;
+            this.picBackGround.Click += new System.EventHandler(this.picBackGround_Click);
+            // 
+            // pic_preView
+            // 
+            this.pic_preView.BackColor = System.Drawing.Color.Khaki;
+            this.pic_preView.Location = new System.Drawing.Point(328, 173);
+            this.pic_preView.Name = "pic_preView";
+            this.pic_preView.Size = new System.Drawing.Size(186, 125);
+            this.pic_preView.TabIndex = 2;
+            this.pic_preView.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("SimSun", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(326, 143);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "下一個方塊：";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("SimSun", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(326, 90);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "分数：";
+            // 
+            // t_score
+            // 
+            this.t_score.AutoSize = true;
+            this.t_score.Font = new System.Drawing.Font("SimSun", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.t_score.ForeColor = System.Drawing.Color.Green;
+            this.t_score.Location = new System.Drawing.Point(370, 113);
+            this.t_score.Name = "t_score";
+            this.t_score.Size = new System.Drawing.Size(16, 16);
+            this.t_score.TabIndex = 4;
+            this.t_score.Text = "0";
+            // 
+            // 游戏设置ToolStripMenuItem
+            // 
+            this.游戏设置ToolStripMenuItem.Name = "游戏设置ToolStripMenuItem";
+            this.游戏设置ToolStripMenuItem.Size = new System.Drawing.Size(32, 19);
+            // 
+            // 速度设置ToolStripMenuItem
+            // 
+            this.速度设置ToolStripMenuItem.Name = "速度设置ToolStripMenuItem";
+            this.速度设置ToolStripMenuItem.Size = new System.Drawing.Size(32, 19);
+            // 
+            // 较慢ToolStripMenuItem
+            // 
+            this.较慢ToolStripMenuItem.Name = "较慢ToolStripMenuItem";
+            this.较慢ToolStripMenuItem.Size = new System.Drawing.Size(32, 19);
+            // 
+            // 慢ToolStripMenuItem
+            // 
+            this.慢ToolStripMenuItem.Name = "慢ToolStripMenuItem";
+            this.慢ToolStripMenuItem.Size = new System.Drawing.Size(32, 19);
+            // 
+            // 快ToolStripMenuItem
+            // 
+            this.快ToolStripMenuItem.Name = "快ToolStripMenuItem";
+            this.快ToolStripMenuItem.Size = new System.Drawing.Size(32, 19);
+            // 
+            // 较快ToolStripMenuItem
+            // 
+            this.较快ToolStripMenuItem.Name = "较快ToolStripMenuItem";
+            this.较快ToolStripMenuItem.Size = new System.Drawing.Size(32, 19);
+            // 
+            // 非常快ToolStripMenuItem
+            // 
+            this.非常快ToolStripMenuItem.Name = "非常快ToolStripMenuItem";
+            this.非常快ToolStripMenuItem.Size = new System.Drawing.Size(32, 19);
+            // 
+            // 背景颜色设置ToolStripMenuItem
+            // 
+            this.背景颜色设置ToolStripMenuItem.Name = "背景颜色设置ToolStripMenuItem";
+            this.背景颜色设置ToolStripMenuItem.Size = new System.Drawing.Size(32, 19);
+            // 
+            // 方块颜色设置ToolStripMenuItem
+            // 
+            this.方块颜色设置ToolStripMenuItem.Name = "方块颜色设置ToolStripMenuItem";
+            this.方块颜色设置ToolStripMenuItem.Size = new System.Drawing.Size(32, 19);
+            // 
+            // 恢复默认设置ToolStripMenuItem
+            // 
+            this.恢复默认设置ToolStripMenuItem.Name = "恢复默认设置ToolStripMenuItem";
+            this.恢复默认设置ToolStripMenuItem.Size = new System.Drawing.Size(32, 19);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(6, 6);
+            // 
+            // 退出ToolStripMenuItem
+            // 
+            this.退出ToolStripMenuItem.Name = "退出ToolStripMenuItem";
+            this.退出ToolStripMenuItem.Size = new System.Drawing.Size(32, 19);
+            // 
+            // 控制ToolStripMenuItem
+            // 
+            this.控制ToolStripMenuItem.Name = "控制ToolStripMenuItem";
+            this.控制ToolStripMenuItem.Size = new System.Drawing.Size(32, 19);
+            // 
+            // 开始ToolStripMenuItem
+            // 
+            this.开始ToolStripMenuItem.Name = "开始ToolStripMenuItem";
+            this.开始ToolStripMenuItem.Size = new System.Drawing.Size(32, 19);
+            // 
+            // 暂停ToolStripMenuItem1
+            // 
+            this.暂停ToolStripMenuItem1.Name = "暂停ToolStripMenuItem1";
+            this.暂停ToolStripMenuItem1.Size = new System.Drawing.Size(32, 19);
+            // 
+            // 结束ToolStripMenuItem
+            // 
+            this.结束ToolStripMenuItem.Name = "结束ToolStripMenuItem";
+            this.结束ToolStripMenuItem.Size = new System.Drawing.Size(32, 19);
+            // 
+            // 重新开始ToolStripMenuItem
+            // 
+            this.重新开始ToolStripMenuItem.Name = "重新开始ToolStripMenuItem";
+            this.重新开始ToolStripMenuItem.Size = new System.Drawing.Size(32, 19);
+            // 
+            // 帮助ToolStripMenuItem
+            // 
+            this.帮助ToolStripMenuItem.Name = "帮助ToolStripMenuItem";
+            this.帮助ToolStripMenuItem.Size = new System.Drawing.Size(32, 19);
+            // 
+            // 关于ToolStripMenuItem
+            // 
+            this.关于ToolStripMenuItem.Name = "关于ToolStripMenuItem";
+            this.关于ToolStripMenuItem.Size = new System.Drawing.Size(32, 19);
+            // 
+            // 操作说明ToolStripMenuItem
+            // 
+            this.操作说明ToolStripMenuItem.Name = "操作说明ToolStripMenuItem";
+            this.操作说明ToolStripMenuItem.Size = new System.Drawing.Size(32, 19);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // msg
+            // 
+            this.msg.AutoSize = true;
+            this.msg.BackColor = System.Drawing.Color.White;
+            this.msg.Font = new System.Drawing.Font("SimSun", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.msg.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.msg.Location = new System.Drawing.Point(126, 173);
+            this.msg.Name = "msg";
+            this.msg.Size = new System.Drawing.Size(39, 20);
+            this.msg.TabIndex = 7;
+            this.msg.Text = "msg";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(417, 24);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 12);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "重 新 開 始";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(417, 55);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(62, 12);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "暫           停";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(417, 90);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(62, 12);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "繼 續 遊 戲";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("SimSun", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label6.ForeColor = System.Drawing.Color.Black;
+            this.label6.Location = new System.Drawing.Point(326, 40);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(44, 12);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "速度：";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("SimSun", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label7.ForeColor = System.Drawing.Color.Green;
+            this.label7.Location = new System.Drawing.Point(325, 64);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(16, 16);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "0";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(417, 117);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(62, 12);
+            this.label8.TabIndex = 13;
+            this.label8.Text = "結           束";
+            this.label8.Click += new System.EventHandler(this.label8_Click);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(326, 314);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(162, 180);
+            this.label9.TabIndex = 14;
+            this.label9.Text = "控制：\r\n\r\n“←”：向左移動方塊\r\n\r\n“→”：向右移動方塊\r\n\r\n“↓”：向下移動方塊\r\n\r\n“↑”：旋转方塊\r\n\r\n“空格”：向下加速移動方塊\r\n\r\n“En" +
+    "ter”：繼續遊戲&開始遊戲\r\n\r\n“Backspace”：暫停";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(528, 502);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.t_score);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pic_preView);
+            this.Controls.Add(this.picBackGround);
+            this.Controls.Add(this.msg);
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "俄羅斯方塊";
+            this.Activated += new System.EventHandler(this.Form1_Activated);
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.picBackGround)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_preView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picBackGround;
+        private System.Windows.Forms.PictureBox pic_preView;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label t_score;
+        private System.Windows.Forms.ToolStripMenuItem 游戏设置ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 速度设置ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 背景颜色设置ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem 退出ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 帮助ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 关于ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 操作说明ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 控制ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 开始ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 暂停ToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem 结束ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 重新开始ToolStripMenuItem;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label msg;
+        private System.Windows.Forms.ToolStripMenuItem 方块颜色设置ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 较慢ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 慢ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 快ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 较快ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 非常快ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 恢复默认设置ToolStripMenuItem;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+    }
+}
+
